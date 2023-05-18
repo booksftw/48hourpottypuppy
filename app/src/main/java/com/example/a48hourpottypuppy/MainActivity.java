cf1a7f50b5dd130fb44d9af717b4bbfddebbc232
Nicholas Zachary <39927058+booksftw@users.noreply.github.com>
2023-05-18 02:32:01 -0700

    Init

---
+++ app/src/main/java/com/example/a48hourpottypuppy/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.a48hourpottypuppy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button loginBtn = findViewById(R.id.buttonLogin);
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d("MAIN ACTIVITY", String.valueOf(view));
+                Intent loginPage = new Intent(getApplicationContext() , ActivityLogin.class);
+                startActivity(loginPage);
+            }
+        });
+
+        Button signupBtn = findViewById(R.id.buttonSignup);
+        signupBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent signupPage = new Intent(getApplicationContext() , ActivitySignUp.class);
+                startActivity(signupPage);
+            }
+        });
+    }
+}
